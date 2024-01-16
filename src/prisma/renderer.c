da3fbc8ad6d5428a2edeb2db6db3beaa6e68bdd9
wervin <fruitaccount@live.com>
2024-01-16 22:49:05 +0100

    Update CMakeLists.txt: Use ExternalProject instead of FetchContent

--- src/prisma/renderer.c
+++ src/prisma/renderer.c
@@ -8,12 +8,8 @@
 
 #include <vulkan/vulkan.h>
 #include <cglm/cglm.h>
-#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
-#define CIMGUI_DEFINE_ENUMS_AND_STRUCTS
 #include <cimgui.h>
-#define CIMGUI_USE_GLFW
-#define CIMGUI_USE_VULKAN
 #include <cimgui_impl.h>
 
 #include <sake/macro.h>
@@ -727,34 +723,6 @@ enum prisma_error prisma_renderer_init_ui(void)
     PRISMA_LOG_ERROR(PRISMA_ERROR_VK, "Failed to instantiate Vulkan for ImGUI");
     return PRISMA_ERROR_VK;
   }
-
-  VkCommandBufferAllocateInfo allocInfo = {0};
-  allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-  allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-  allocInfo.commandPool = _backend.ui.vk_commandpool;
-  allocInfo.commandBufferCount = 1;
-
-  VkCommandBuffer commandbuffer;
-  vkAllocateCommandBuffers(_backend.device.vk_device, &allocInfo, &commandbuffer);
-
-  VkCommandBufferBeginInfo beginInfo = {0};
-  beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-  beginInfo.flags = VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT;
-
-  vkBeginCommandBuffer(commandbuffer, &beginInfo);
-
-  ImGui_ImplVulkan_CreateFontsTexture(commandbuffer);
-
-  vkEndCommandBuffer(commandbuffer);
-
-  VkSubmitInfo submitInfo = {0};
-  submitInfo.sType = VK_STRUCTURE_TYPE_SUBMIT_INFO;
-  submitInfo.commandBufferCount = 1;
-  submitInfo.pCommandBuffers = &commandbuffer;
-
-  vkQueueSubmit(_backend.device.vk_graphic_queue, 1, &submitInfo, VK_NULL_HANDLE);
-  vkQueueWaitIdle(_backend.device.vk_graphic_queue);
-  vkFreeCommandBuffers(_backend.device.vk_device, _backend.ui.vk_commandpool, 1, &commandbuffer);
 
   return PRISMA_ERROR_NONE;
 }
